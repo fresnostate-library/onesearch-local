63ba2e08601a0bc7dfae4c06892ce6b6ffd91f58
David Palmquist <quist00@me.com>
2017-06-23 10:58:22 -0700

    enable production mode by default
    addresses issue #9 comment out call to enableProdMode() in app.module.ts for development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { Location } from '@angular/common';
-
 import { AppComponent } from './app.component';
+
+enableProdMode();
 
 @NgModule({
   declarations: [
